4fe6877ef73f9383e1806354a7a684a27012abe3
rahul-chakradhari <rahulchakradhari10september@gmail.com>
2025-12-25 18:43:45 +0530

    Enable inter-service communication using RestTemplate

---
+++ parking-lot/microservices/slot-service/src/test/java/com/rahul/parking/slot_service/SlotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.parking.slot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
