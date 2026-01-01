10e32fced49015d11f43e901da322103a5fd277c
rahul-chakradhari <rahulchakradhari10september@gmail.com>
2026-01-01 20:48:58 +0530

    Added timeout support in user-service and parking-service for fault tolerance

--- microservices/parking-service/src/main/java/com/rahul/parking/parking_service/ParkingServiceApplication.java
+++ microservices/parking-service/src/main/java/com/rahul/parking/parking_service/ParkingServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,7 +13,10 @@ public class ParkingServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		//implementing timeout functionality
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(5000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 
--- microservices/user-service/src/main/java/com/rahul/parking/user_service/UserServiceApplication.java
+++ microservices/user-service/src/main/java/com/rahul/parking/user_service/UserServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,7 +13,10 @@ public class UserServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		//implementing time out functionality
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(5000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	public static void main(String[] args) {
